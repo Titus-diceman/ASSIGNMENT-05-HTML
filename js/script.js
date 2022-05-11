2eb6cf97342d179e9015d6b3510a4647e7fd9999
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-11 17:51:15 -0400

    changed javascript once agian

--- js/script.js
+++ js/script.js
@@ -21,18 +21,18 @@ function buttonClicked() {
 
   var integer = document.getElementById("integer").value
   var currentSymbol = "negative"
-  var bottomNumber = 3
-  var answer = 4
+  var bottomNumber = 1
+  var answer = 0
 
   if (integer > 0) {
     for (let counter = 0; counter < integer; counter++) {
       if (currentSymbol == "negative") {
         bottomNumber = bottomNumber + 2
-        answer = answer - (4 / bottomNumber)
+        answer = answer + (4 / bottomNumber)
         currentSymbol = "positive"
       } else if (currentSymbol == "positive") {
         bottomNumber = bottomNumber + 2
-        answer = answer + (4 / bottomNumber)
+        answer = answer - (4 / bottomNumber)
         currentSymbol = "negative"
         }
       }
