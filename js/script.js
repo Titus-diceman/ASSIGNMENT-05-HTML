4f8eb2897ee65a32187a09f9ec643f93e6af6cc8
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-11 13:07:04 -0400

    changed toFixed(10)

--- js/script.js
+++ js/script.js
@@ -36,5 +36,5 @@ function buttonClicked() {
     }
     integer = integer - 1
   }
-  document.getElementById("output").innerHTML = answer.toFixed(4)
+  document.getElementById("output").innerHTML = answer.toFixed(10)
 }
